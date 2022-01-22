ac964f7739748aaaebdb1f0a0ae8599dbb3b39a2
saad277 <saadsheikh27@gmail.com>
2022-01-23 00:10:10 +0500

    auth actions

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,2 +1,46 @@
+import { httpRequest, postConfig, getError, getConfig } from "../../utils/requestUtils";
+
 export const ME_SUCCESS = "ME_SUCCESS";
 export const LOG_OUT = "LOG_OUT";
+
+export const login = (payload) => (dispatch) => {
+  return httpRequest
+    .post("/login", payload, postConfig)
+    .then(async (res) => {
+      let token = res.data.Token;
+      await localStorage.setItem("token", token);
+      return dispatch(getMe(token));
+    })
+    .catch((err) => {
+      Snackbar.show({
+        text: getError(err),
+        duration: Snackbar.LENGTH_SHORT,
+      });
+      return Promise.reject(err);
+    });
+};
+
+export const getMe = (token) => (dispatch) => {
+  return httpRequest
+    .get("/user/me", getConfig(token))
+    .then((res) => {
+      dispatch({
+        type: AuthActionType.ME_SUCCESS,
+        payload: { Token: token, ...res.data },
+      });
+
+      return Promise.resolve(res.data);
+    })
+    .catch((err) => {
+      dispatch(logout());
+      return Promise.reject(err);
+    });
+};
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("token");
+
+  dispatch({
+    type: LOG_OUT,
+  });
+};
